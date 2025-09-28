2791635a4da70eaf694898b86f83d99e3e47f7a7
Anmol-Sharma7011 <anmolsharma7011@gmail.com>
2025-09-28 11:26:43 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ import ListShows from "./pages/admin/ListShows";
 import ListBookings from "./pages/admin/ListBookings";
 
 const App = () => {
-  const isAdminRoute = useLocation().pathname.includes("/admin");
+  const isAdminRoute = useLocation().pathname.includes("admin");
 
   return (
     <>
